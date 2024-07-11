9473257e826d814da9761a5065e3c48984914e12
jorgementx <jordims2000@gmail.com>
2024-07-11 21:20:14 +0200

    feat: custom korean keyboard, different learning modes

--- src/components/KeyboardTest.jsx
+++ src/components/KeyboardTest.jsx
@@ -14,7 +14,27 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
 import "./keyboardTest.css";
+
+const customKolayout = {
+  default: [
+    "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+    "{tab} ㅂ ㅈ ㄷ ㄱ ㅅ ㅛ ㅕ ㅑ ㅐ ㅔ [ ] \\",
+    "{lock} ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ ; ' {enter}",
+    "{shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ , . / {shift}",
+    ".com @ {space}",
+  ],
+  shift: [
+    "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+    "{tab} ㅃ ㅉ ㄸ ㄲ ㅆ ㅛ ㅕ ㅑ ㅒ ㅖ { } |",
+    '{lock} ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ : " {enter}',
+    "{shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ < > ? {shift}",
+    ".com @ {space}",
+  ],
+};
 
 function KeyboardTest({ wordList }) {
   const [input, setInput] = useState("");
@@ -23,9 +43,18 @@ function KeyboardTest({ wordList }) {
   // const [theme, setTheme] = useState("default");
   const keyboard = useRef();
   // const [currentWord, setCurrentWord] = useState(wordList[getRandomInt(wordList.length)]);
-  const [currentWord, setCurrentWord] = useState(wordList[getRandomInt(wordList.length)]);
-  const [nextLetter, setNextLetter] = useState(currentWord[0]);
-
+  const lettersTopRow = "ㅂㅈㄷㄱㅅㅛㅕㅑㅐㅔ";
+  const lettersHomeRow = "ㅁㄴㅇㄹㅎㅗㅓㅏㅣ";
+  const lettersBottomRow = "ㅋㅌㅊㅍㅠㅜㅡ";
+  const lettersFullWithoutShift =
+    lettersTopRow + lettersHomeRow + lettersBottomRow;
+  const lettersFull = lettersFullWithoutShift + "ㅃㅉㄸㄲㅆㅒㅖ";
+  let letters = lettersFullWithoutShift;
+  const [currentWord, setCurrentWord] = useState(
+    letters[getRandomInt(letters.length)]
+  );
+  const [highlightNextLetter, setHighlightNextLetter] = useState(true);
+  const [nextLetter, setNextLetter] = useState(currentWord[0]); // TODO: en el caso del coreano hay que separar en jamo (y por lo tanto no va a valer con mirar la longitud del input actual, habrá que contar las teclas pulsadas por ejemplo)
   const handleShift = () => {
     const newLayoutName = layout === "default" ? "shift" : "default";
     setLayout(newLayoutName);
@@ -39,23 +68,28 @@ function KeyboardTest({ wordList }) {
     const input = event.target.value;
     setInput(input);
     keyboard.current.setInput(input);
-    setNextLetter(currentWord[input.length]);
+    // setNextLetter(currentWord[input.length]);
+  };
+
+  const handleHighlightToggle = (value) => {
+    setHighlightNextLetter(value.target.checked);
   };
 
   const handleModeChange = (value) => {
-    switch (value) {
-      case "rndLetters":
-        setMode("rndLetters");
-      case "rndWords":
-        setMode("rndWords");
-        break;
-      case "rndSet":
-        setMode("rndSet");
-        break;
-      default:
-        setMode("rndLetters");
-        break;
-    }
+    setMode(value);
+    // switch (value) {
+    //   case "rndLetters":
+    //     setMode("rndLetters");
+    //   case "rndWords":
+    //     setMode("rndWords");
+    //     break;
+    //   case "rndSet":
+    //     setMode("rndSet");
+    //     break;
+    //   default:
+    //     setMode("rndLetters");
+    //     break;
+    // }
   };
 
   const handleLayoutChange = (value) => {
@@ -87,28 +121,13 @@ function KeyboardTest({ wordList }) {
       case "Korean":
         setLayout("default");
         keyboard.current.setOptions({
-          layout: {
-            default: [
-              "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
-              "{tab} ㅂ ㅈ ㄷ ㄱ ㅅ ㅛ ㅕ ㅑ ㅐ ㅔ [ ] \\",
-              "{lock} ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ ; ' {enter}",
-              "{shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ , . / {shift}",
-              ".com @ {space}",
-            ],
-            shift: [
-              "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
-              "{tab} ㅃ ㅉ ㄸ ㄲ ㅆ ㅛ ㅕ ㅑ ㅒ ㅖ { } |",
-              '{lock} ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ : " {enter}',
-              "{Shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ < > ? {shift}",
-              ".com @ {space}",
-            ],
-          },
+          layout: customKolayout,
         });
         break;
       default:
         setLayout("default");
         keyboard.current.setOptions({
-          layout: enlayout.layout,
+          layout: customKolayout,
         });
     }
   };
@@ -118,7 +137,9 @@ function KeyboardTest({ wordList }) {
       switch (mode) {
         case "rndLetters":
           setInput("");
-          setCurrentWord(wordList[getRandomInt(wordList.length)]);
+          const letter = letters[getRandomInt(letters.length)];
+          setCurrentWord(letter);
+          setNextLetter(letter);
           break;
         case "rndWords":
           setInput("");
@@ -159,7 +180,7 @@ function KeyboardTest({ wordList }) {
             backgroundSize: "40%",
           }}
           className={
-            "w-full px-8 py-4 text-4xl font-semibold border-none  rounded-md mb-2 outline-none focus:outline-rose-300 "
+            "w-full px-8 h-[4.5rem] text-4xl font-semibold border-none text-center rounded-md mb-2 outline-none focus:outline-rose-300 "
           }
           value={input}
           onKeyUp={handleKeyUp}
@@ -172,7 +193,7 @@ function KeyboardTest({ wordList }) {
           layoutName={layout}
           onKeyPress={onKeyPress}
           theme={"hg-theme-default hg-layout-default sakuraTheme"}
-          // layout={eslayout.layout}
+          layout={customKolayout}
           physicalKeyboardHighlight={true}
           physicalKeyboardHighlightPress={true}
           syncInstanceInputs={true}
@@ -182,13 +203,17 @@ function KeyboardTest({ wordList }) {
               buttons: "{shift} {backspace} {tab} @ {bksp} .com {lock} {enter}",
             },
             {
-              class: "highlight-key",
+              class: highlightNextLetter ? "highlight-key" : "",
               buttons: nextLetter,
             },
           ]}
         />
-        <div className="flex gap-2">
-          <Select className="" onValueChange={handleLayoutChange}>
+        <div className="flex gap-2 items-center">
+          <Select
+            className=""
+            onValueChange={handleLayoutChange}
+            defaultValue="Korean"
+          >
             <SelectTrigger className="w-full mt-2 focus:ring-rose-300 focus:ring-offset-base-100">
               <SelectValue placeholder="Keyboard layout" />
             </SelectTrigger>
@@ -200,7 +225,11 @@ function KeyboardTest({ wordList }) {
               <SelectItem value="Korean">Korean</SelectItem>
             </SelectContent>
           </Select>
-          <Select className="" onValueChange={handleModeChange}>
+          <Select
+            className=""
+            onValueChange={handleModeChange}
+            defaultValue="rndLetters"
+          >
             <SelectTrigger className="w-full mt-2 focus:ring-rose-300 focus:ring-offset-base-100">
               <SelectValue placeholder="Exercise mode" />
             </SelectTrigger>
@@ -212,7 +241,40 @@ function KeyboardTest({ wordList }) {
               </SelectItem>
             </SelectContent>
           </Select>
+          {mode === "rndLetters" && (
+            <input
+              type="checkbox"
+              class="toggle toggle-lg tooltip"
+              data-tip="Show/hide letter highlight"
+              checked={highlightNextLetter}
+              onChange={handleHighlightToggle}
+            />
+          )}
         </div>
+        {/* {mode === "rndLetters" && (
+          <RadioGroup defaultValue="option-one">
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="option-one" id="option-one" />
+              <Label htmlFor="option-one">Home row</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="option-two" id="option-two" />
+              <Label htmlFor="option-two">Top row</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="option-two" id="option-two" />
+              <Label htmlFor="option-two">Bottom row</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="option-two" id="option-two" />
+              <Label htmlFor="option-two">Full Keyboard (without shift)</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="option-two" id="option-two" />
+              <Label htmlFor="option-two">Full keyboard (with shift)</Label>
+            </div>
+          </RadioGroup>
+        )} */}
       </div>
     </>
   );
